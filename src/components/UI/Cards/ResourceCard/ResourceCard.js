68caa4a35a0a312539d8d9bb0322f14a411a78b4
elluis <elluis.codes@gmail.com>
2022-07-06 21:53:02 -0500

    just added menu jsx to Navbar - working on the connectivity now -

--- src/components/UI/Cards/ResourceCard/ResourceCard.js
+++ src/components/UI/Cards/ResourceCard/ResourceCard.js
@@ -2,16 +2,13 @@ import React from "react";
 import "./ResourceCard.css";
 
 
-
+// this will append the 'card' class name and allow use this component as a wrap
 const ResourceCard = (props) => {
     const classes = 'card '  + props.className;
-
-
 
     return (
         <div className={classes}>{props.children}</div>
     )
-
-}
+};
 
 export default ResourceCard;
---
+++ src/components/UI/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import "./Navbar.css";
+
+
+
+
+const Navbar = () => {
+
+
+    return (
+        <div className="navbar">
+            <div className="menu">
+                <ul className="menu__list">
+                    <li className="links">links</li>
+                    <li className="links">links</li>
+                    <li className="portals">portals</li>
+                    <li className="schedules">schedules</li>
+                </ul>
+            </div>
+        </div>
+
+    )
+}
+
+
+export default Navbar;
