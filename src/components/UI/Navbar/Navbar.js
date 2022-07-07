9114a61617614818c42b8986b80d4ee11a635547
elluis <elluis.codes@gmail.com>
2022-07-07 08:54:01 -0500

    very rudimentary implementation of a responsive navbar is functional -
    - thoughts - there are several ways to implement responsiveness with react -
    lets build some generic functions that use different ways to achieve the same functionality -
    for reference and experimentation purposes -

--- src/components/UI/Navbar/Navbar.js
+++ src/components/UI/Navbar/Navbar.js
@@ -5,11 +5,21 @@ import { ReactComponent as NasaWorm } from "../../../assets/images/NASA_Worm_log
 const Navbar = () => {
 
     return (
+
         <nav className="navbar">
             <a href="/" className="logo">
                 <NasaWorm className="nasaWorm" />
             </a>
             <button className="rocket"><span className="material-symbols-outlined">rocket</span></button>
+            <div className="nav-menu">
+                <ul className="nav-menu-list">
+                    <li className="nav-menu-listItem"><a href="/home" className="nav-menu-anchor">home</a></li>
+                    <li className="nav-menu-listItem"><a href="/about" className="nav-menu-anchor">about</a></li>
+                    <li className="nav-menu-listItem"><a href="/contact" className="nav-menu-anchor">contact</a></li>
+                    <li className="nav-menu-listItem"><a href="/links" className="nav-menu-anchor">links</a></li>
+                    <li className="nav-menu-listItem"><a href="/resources" className="nav-menu-anchor">resources</a></li>
+                </ul>
+            </div>
         </nav>
     )
 }
